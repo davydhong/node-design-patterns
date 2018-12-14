7a23d00225eebc3675fc637851abdb6da571e0cf
davydhong <davydhong@gmail.com>
2018-12-14 00:06:02 -0800

    proxy

---
+++ stuctural/proxy.1/FS_Proxy.js
@@ -0,0 +1,23 @@
+class FS_Proxy {
+  constructor(fs_subject) {
+    // this where you copy the method of the subject
+    this.fs = fs_subject;
+  }
+
+  readFile(path, format, callback) {
+    if (!path.match(/.md$|.MD$/)) {
+      return callback(new Error('Can only read Markdown files.'));
+    }
+
+    // this is the invocation of the copied method.
+    this.fs.readFile(path, format, (error, contents) => {
+      if (error) {
+        console.error(error);
+        return callback(error);
+      }
+      return callback(null, contents);
+    });
+  }
+}
+
+module.exports = FS_Proxy;
--- stuctural/proxy.1/index.js
+++ stuctural/proxy.1/index.js
@@ -1,21 +1,27 @@
-var fs = require('fs');
-var path = require('path');
+// ! Provide a surrogate or placeholder for another object (much more bigger/expensive object) to control access to it.
+// ? Useful for (1) managing expensive objects, (2) managing a remote resource, (3) provide data validation of input, (4) provide security, (5) cache data, (6) even log actions. Just like the adapter pattern, a proxy must provide the same interface as the subject.
 
-var txtFile = path.join(__dirname, 'Readme.txt');
-var mdFile = path.join(__dirname, 'Readme.md');
+// ! goal: restrict reading of a file
 
-var result = (error, contents) => {
+// let fs = require('fs');
 
-    if (error) {
-        console.log('\x07');
-        console.error(error);
-        process.exit(0);
-    }
+const path = require('path');
+const FS_Proxy = require('./FS_Proxy');
+const fs = new FS_Proxy(require('fs'));
 
-    console.log('reading file...');
-    console.log(contents);
+// const txtFile = path.join(__dirname, 'Readme.txt');
+const mdFile = path.join(__dirname, 'Readme.md');
 
-}
+const result = (error, contents) => {
+  if (error) {
+    console.log('\x07');
+    console.error(error);
+    process.exit(0);
+  }
 
-fs.readFile(txtFile, 'UTF-8', result);
+  console.log('reading file...');
+  console.log(contents);
+};
+
+// fs.readFile(txtFile, 'UTF-8', result);
 fs.readFile(mdFile, 'UTF-8', result);
